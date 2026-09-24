1ecacc88376c8cde059cbbd5ed58649a5d75371b
NNS11 <nicolasnunes915@gmail.com>
2026-09-24 17:20:09 -0300

    adicionei Status de documento

--- src/telas/TelaDeCadastro.java
+++ src/telas/TelaDeCadastro.java
@@ -30,10 +30,22 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
             txtEndereco.setText(rs.getString("Endereco_Cliente"));
             txtCidade.setText(rs.getString("Cidade_Cliente"));
             txtUF.setText(rs.getString("UF_Cliente"));
-            txtCPF.setText(rs.getString("CPF_Cliente"));
+            txtDoc.setText(rs.getString("Documento_Cliente"));
+            if (rs.getString("status").equals("PF")) {
+    rdPF.setSelected(true);
+} else if (rs.getString("status").equals("PJ")) {
+    rdPJ.setSelected(true);
+}
             txtTelefone.setText(rs.getString("Telefone_Cliente"));
             txtDataNasc.setText(rs.getString("Data_Nasc_Cliente"));
-
+            if (rdPF.isSelected()) {
+    pst.setString(8, "PF");
+} else if (rdPJ.isSelected()) {
+    pst.setString(8, "PJ");
+} else {
+    JOptionPane.showMessageDialog(null, "Selecione PF ou PJ.");
+    return;
+}
         } else {
 
             JOptionPane.showMessageDialog(null,
@@ -50,10 +62,10 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
    private void adicionar(){
   
 
-    String sql = "INSERT INTO TB_Clientes "
-            + "(Nome_Cliente, Endereco_Cliente, Cidade_Cliente, UF_Cliente, "
-            + "CPF_Cliente, Telefone_Cliente, Data_Nasc_Cliente) "
-            + "VALUES (?, ?, ?, ?, ?, ?, ?)";
+   String sql = "INSERT INTO TB_Clientes "
+        + "(Nome_Cliente, Endereco_Cliente, Cidade_Cliente, UF_Cliente, "
+        + "documento_cliente, Telefone_Cliente, Data_Nasc_Cliente, status) "
+        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
     try {
 
@@ -63,9 +75,17 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
         pst.setString(2, txtEndereco.getText());
         pst.setString(3, txtCidade.getText());
         pst.setString(4, txtUF.getText());
-        pst.setString(5, txtCPF.getText());
+        pst.setString(5, txtDoc.getText());
         pst.setString(6, txtTelefone.getText());
         pst.setString(7, txtDataNasc.getText());
+        if (rdPF.isSelected()) {
+    pst.setString(8, "PF");
+} else if (rdPJ.isSelected()) {
+    pst.setString(8, "PJ");
+} else {
+    JOptionPane.showMessageDialog(null, "Selecione PF ou PJ.");
+    return;
+}
 
         int adicionado = pst.executeUpdate();
 
@@ -78,7 +98,7 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
             txtEndereco.setText(null);
             txtCidade.setText(null);
             txtUF.setText(null);
-            txtCPF.setText(null);
+            txtDoc.setText(null);
             txtTelefone.setText(null);
             txtDataNasc.setText(null);
         }
@@ -96,9 +116,10 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
             + "Endereco_Cliente = ?, "
             + "Cidade_Cliente = ?, "
             + "UF_Cliente = ?, "
-            + "CPF_Cliente = ?, "
+            + "documento_cliente = ?, "
             + "Telefone_Cliente = ?, "
             + "Data_Nasc_Cliente = ? "
+            + "status = ? "
             + "WHERE ID_Cliente = ?";
 
     try {
@@ -109,10 +130,20 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
         pst.setString(2, txtEndereco.getText());
         pst.setString(3, txtCidade.getText());
         pst.setString(4, txtUF.getText());
-        pst.setString(5, txtCPF.getText());
-        pst.setString(6, txtTelefone.getText());
-        pst.setString(7, txtDataNasc.getText());
-        pst.setString(8, txtId.getText());
+       pst.setString(5, txtDoc.getText());
+pst.setString(6, txtTelefone.getText());
+pst.setString(7, txtDataNasc.getText());
+
+if (rdPF.isSelected()) {
+    pst.setString(8, "PF");
+} else if (rdPJ.isSelected()) {
+    pst.setString(8, "PJ");
+} else {
+    JOptionPane.showMessageDialog(null, "Selecione PF ou PJ.");
+    return;
+}
+
+pst.setString(9, txtId.getText());
 
         int alterado = pst.executeUpdate();
 
@@ -163,9 +194,13 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
                 txtEndereco.setText(null);
                 txtCidade.setText(null);
                 txtUF.setText(null);
-                txtCPF.setText(null);
+                txtDoc.setText(null);
+                rdPF.setSelected(false); 
+                rdPJ.setSelected(false);  
                 txtTelefone.setText(null);
                 txtDataNasc.setText(null);
+              
+
 
             } else {
 
@@ -185,6 +220,7 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
     private void initComponents() {
 
         jComboBox1 = new javax.swing.JComboBox<>();
+        buttonGroup1 = new javax.swing.ButtonGroup();
         ID = new javax.swing.JLabel();
         nome = new javax.swing.JLabel();
         email = new javax.swing.JLabel();
@@ -199,13 +235,15 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
         btnApagar = new javax.swing.JButton();
         btnEdit = new javax.swing.JButton();
         txtUF = new javax.swing.JTextField();
-        txtCPF = new javax.swing.JTextField();
+        txtDoc = new javax.swing.JTextField();
         txtTelefone = new javax.swing.JTextField();
         txtDataNasc = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        rdPF = new javax.swing.JRadioButton();
+        rdPJ = new javax.swing.JRadioButton();
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
@@ -273,11 +311,17 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
 
         jLabel2.setText("UF:");
 
-        jLabel3.setText("CPF:");
+        jLabel3.setText("Documento");
 
         jLabel4.setText("Telefone:");
 
         jLabel5.setText("Data Nasc");
+
+        buttonGroup1.add(rdPF);
+        rdPF.setText("PF");
+
+        buttonGroup1.add(rdPJ);
+        rdPJ.setText("PJ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -285,21 +329,6 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(250, 250, 250)
-                        .addComponent(jLabel1)
-                        .addGap(0, 123, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnVisualizar)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(btnApagar, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(btnAdd)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(124, 124, 124)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -313,24 +342,44 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
                             .addComponent(jLabel3)
                             .addComponent(jLabel4)
                             .addComponent(email))
-                        .addGap(32, 32, 32)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtCidade)
-                            .addComponent(txtEndereco)
-                            .addComponent(txtNome)
-                            .addComponent(txtId)
-                            .addComponent(txtUF)
-                            .addComponent(txtCPF)
-                            .addComponent(txtTelefone)
-                            .addComponent(txtDataNasc))))
+                        .addGap(32, 32, 32))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(rdPF)
+                        .addGap(18, 18, 18)))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(rdPJ)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnVisualizar)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btnApagar, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(btnAdd)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(btnEdit, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addComponent(txtCidade)
+                    .addComponent(txtEndereco)
+                    .addComponent(txtNome)
+                    .addComponent(txtId)
+                    .addComponent(txtUF)
+                    .addComponent(txtDoc)
+                    .addComponent(txtTelefone)
+                    .addComponent(txtDataNasc))
                 .addGap(110, 110, 110))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(222, 222, 222))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(20, 20, 20)
+                .addGap(18, 18, 18)
                 .addComponent(jLabel1)
-                .addGap(40, 40, 40)
+                .addGap(42, 42, 42)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(ID))
@@ -352,7 +401,7 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
                     .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtCPF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtDoc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
@@ -371,6 +420,12 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
                     .addComponent(btnVisualizar)
                     .addComponent(btnApagar))
                 .addGap(31, 31, 31))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(rdPF)
+                    .addComponent(rdPJ))
+                .addGap(47, 47, 47))
         );
 
         pack();
@@ -416,6 +471,7 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
     private javax.swing.JButton btnApagar;
     private javax.swing.JButton btnEdit;
     private javax.swing.JButton btnVisualizar;
+    private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel email;
     private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
@@ -424,10 +480,12 @@ public class TelaDeCadastro extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel nome;
+    private javax.swing.JRadioButton rdPF;
+    private javax.swing.JRadioButton rdPJ;
     private javax.swing.JLabel senha;
-    private javax.swing.JTextField txtCPF;
     private javax.swing.JTextField txtCidade;
     private javax.swing.JTextField txtDataNasc;
+    private javax.swing.JTextField txtDoc;
     private javax.swing.JTextField txtEndereco;
     private javax.swing.JTextField txtId;
     private javax.swing.JTextField txtNome;
